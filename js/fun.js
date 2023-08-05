b52a617c2d665502420d181668fd05bd3e64315b
wangxin-tian <3106532244@qq.com>
2023-08-05 14:27:50 +0800

    Deloyed by Github Actions: 2023-08--05 14:27:49

--- js/fun.js
+++ js/fun.js
@@ -1,4 +1,7 @@
-console.log('此博客项目来自BF-OK(https://github.com/TIANLI0/BF-OK)');
+console.log('此博客项目借鉴自BF-OK(https://github.com/TIANLI0/BF-OK)');
+console.log('这是经过TIANLI魔改的butterfly主题，\n我的计划是使用这个静态页面，\n给它搭建一个后台并且尝试自己的修改');
+console.log('%c后台搭建：%c in progress',"padding: 5px 0","color: #fff; background: #00a6ff; padding: 5px 0");
+console.log('%c页面修改：%c in progress',"padding: 5px 0","color: #fff; background: #00a6ff; padding: 5px 0");
 console.log(`
   ===================================================================
                                                                      
@@ -12,6 +15,6 @@ console.log(`
                     4.0.0 BF-OK 1.0.0 By Tianli                       
   ===================================================================
 `);
-console.log("%c    ","background: url(https://img1.tianli0.top/phlogo.png) no-repeat left center;font-size: 200px;","\n");
-console.log("\n %c 隐私政策 %c https://tianli-blog.club/privacy/ \n","color: #ffffff; background: #00a6ff; padding:5px 0;","background: #fadfa3; padding:5px 0;")
+// console.log("%c    ","background: url(https://img1.tianli0.top/phlogo.png) no-repeat left center;font-size: 200px;","\n");
+console.log("\n %c 隐私政策 %c https://dhxia.xyz/privacy/ \n","color: #ffffff; background: #00a6ff; padding:5px 0;","background: #fadfa3; padding:5px 0;")
 console.log("\n %c JSD静态资源CDN %c https://cdn1.tianli0.top/ \n","color: #ffffff; background: #00a6ff; padding:5px 0;","background: #fadfa3; padding:5px 0;")
